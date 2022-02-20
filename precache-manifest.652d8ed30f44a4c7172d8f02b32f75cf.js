6e112cf02d703b0ba5ae9cebdf77e14a62c5f696
JohnVemula1998 <johnvemula7771998@gmail.com>
2022-02-20 15:09:13 +0530

    Updates

---
+++ precache-manifest.652d8ed30f44a4c7172d8f02b32f75cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee70f2f37d368fa19137",
+    "url": "/tic-tac-toe_React_App/static/css/main.506c85ea.chunk.css"
+  },
+  {
+    "revision": "ee70f2f37d368fa19137",
+    "url": "/tic-tac-toe_React_App/static/js/main.a51d22ab.chunk.js"
+  },
+  {
+    "revision": "2d70df784c1e27b5ad64",
+    "url": "/tic-tac-toe_React_App/static/js/runtime~main.e5bb8304.js"
+  },
+  {
+    "revision": "8783b8ae0ca988025217",
+    "url": "/tic-tac-toe_React_App/static/js/2.dfab15d6.chunk.js"
+  },
+  {
+    "revision": "604c5b27a93ed73d059ea2b4e0e2ce40",
+    "url": "/tic-tac-toe_React_App/index.html"
+  }
+];
